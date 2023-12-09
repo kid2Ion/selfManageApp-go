9219a72e0d92d86d4ea1e8022762241853d43c47
kid2Ion <hiroki.kondo@zuuonline.com>
2023-12-09 19:35:59 +0900

    CORS

--- main.go
+++ main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/kid2Ion/selfManageApp-go/di"
 	"github.com/kid2Ion/selfManageApp-go/router"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	fmt.Println("start server")
 	helloHandler := di.InjectHandler()
 	e := echo.New()
+
+	// 単純でないリクエストを許可
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:8080", os.Getenv("FE_URL")},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept,
+			echo.HeaderAccessControlAllowHeaders, echo.HeaderXCSRFToken},
+		AllowMethods:     []string{http.MethodGet, http.MethodPut, http.MethodPost, http.MethodDelete},
+		AllowCredentials: true,
+	}))
+
+	//todo: IPアクセス制限
+	// CSRFでやる？
+
 	router.InitHelloRouter(e, helloHandler)
 	e.Logger.Fatal(e.Start(":8080"))
 }
